84e6ac4812422761e9381465ae5152e9af10e54b
taegyu jang <taegyu1500@gmail.com>
2025-09-02 11:16:48 +0900

    문제풀이

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,26 @@
 def add(a, b):
-    pass
+    return a+b
 
 
 def sub(a, b):
-    pass
+    return a-b
 
 
 def mul(a, b):
-    pass
+    return a*b
 
 
 def div(a, b):
-    pass
+    return a/b
 
 
 def power(base, pow):
-    pass
+    return base ** pow
 
 
 def square(base):
-    pass
+    return base ** 2
 
 
 def greet(이름="낯선자", 나이=20):
-    pass
+    return f"안녕하신가 {이름}!" if 나이 == 20 else f"안녕하십니까 {이름}!" if 나이 > 20 else f"안녕 {이름}!"
